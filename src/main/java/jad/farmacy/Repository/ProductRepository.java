57313b328e60fa0a7a60f55152d38eb3818f6bf9
Fercoold <fernandomaldonado751@gmail.com>
2024-09-07 15:21:56 -0600

    Ventas

--- src/main/java/jad/farmacy/Repository/ProductRepository.java
+++ src/main/java/jad/farmacy/Repository/ProductRepository.java
@@ -6,13 +6,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
     @Query(value = "SELECT * FROM PRODUCTS WHERE QR_CODE = :CODE",nativeQuery = true)
-    Product findByBarcode(@Param("CODE") String code);
+    List<Product> findByBarcode(@Param("CODE") String code);
 
     @Query(value = "SELECT * FROM PRODUCTS WHERE STORE_ID = :STOREID",nativeQuery = true)
     List<Product> findAllByStore(@Param("STOREID") long storeID);
+
+    @Query("SELECT p FROM Product p WHERE p.expirationDate BETWEEN :today AND :twoMonthsLater")
+    List<Product> findProductsExpiringInNextTwoMonths(@Param("today") LocalDate today, @Param("twoMonthsLater") LocalDate twoMonthsLater);
 
 }
